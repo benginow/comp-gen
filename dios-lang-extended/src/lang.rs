4e91bb96606872c57874e92c75f3f4d13a7805f8
benginow <jbenginow@gmail.com>
2024-02-05 16:37:34 -0600

    p

--- dios-lang-extended/src/lang.rs
+++ dios-lang-extended/src/lang.rs
@@ -10,10 +10,78 @@ use std::str::FromStr;
 pub enum Value {
     // starts with i
     Int(i64),
-    // starts with <
-    List((Value, List)),
-    // tail is no longer unwrappable
+    // could also define this also List(Value, List), but I'm not positive we want true recursion
+    List((Value, Vec<Value>)),
+    // tail is no longer unwrappable -> just a vector of values
     Tail(Vec<Value>),
     // starts with b
     Bool(bool),
+}
+
+egg::define_language! {
+    #[derive(Serialize, Deserialize)] 
+    pub enum VecLang {
+        // Id is a key to identify EClasses within an EGraph, represents
+        // children nodes
+        "+" = Add([Id; 2]),
+        "*" = Mul([Id; 2]),
+        "-" = Minus([Id; 2]),
+        "/" = Div([Id; 2]),
+
+        "or" = Or([Id; 2]),
+        "&&" = And([Id; 2]),
+        "ite" = Ite([Id; 3]),
+        "<" = Lt([Id; 2]),
+        "sqrtsgn" = SqrtSgn([Id; 2]),
+
+        "sgn" = Sgn([Id; 1]),
+        "sqrt" = Sqrt([Id; 1]),
+        "neg" = Neg([Id; 1]),
+
+        // Lists have a variable number of elements
+        "List" = List(Box<[Id]>),
+
+        // Vectors have width elements
+        "Vec" = Vec(Box<[Id]>),
+
+        // Vector operations that take 1 vector of inputs
+        "VecNeg" = VecNeg([Id; 1]),
+        "VecSqrt" = VecSqrt([Id; 1]),
+        "VecSgn" = VecSgn([Id; 1]),
+        // Vector sum to unary
+        "VecSum" = VecSum([Id;1])
+
+        // Vector operations that take 2 vectors of inputs
+        "VecAdd" = VecAdd([Id; 2]),
+        "VecMinus" = VecMinus([Id; 2]),
+        "VecMul" = VecMul([Id; 2]),
+        "VecDiv" = VecDiv([Id; 2]),
+        "VecMulSgn" = VecMulSgn([Id; 2]),
+        "VecSqrtSgn" = VecSqrtSgn([Id; 2]),
+
+        // Vector operations that take 3 vectors of inputs
+        // MAC takes 3 lists: acc, v1, v2
+        "VecMAC" = VecMAC([Id; 3]),
+        "VecMULS" = VecMULS([Id; 3]),
+
+        Const(Value),
+
+        // language items are parsed in order, and we want symbol to
+        // be a fallback, so we put it last.
+        // `Symbol` is an egg-provided interned string type
+        Symbol(egg::Symbol),
+    }
+}
+
+impl SynthLanguage for lang::VecLang {
+    type Constant = lang::Value;
+
+    fn eval<'a, F>(&'a self, cvec_len: usize, mut get_cvec: F) -> CVec<Self>
+    where
+        F: FnMut(&'a Id) -> &'a CVec<Self>,
+    {
+        match self {
+
+        }
+    }
 }
